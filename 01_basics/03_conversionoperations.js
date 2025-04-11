e96d36e80356592d81cb0414d03e065780d3f9f1
AKSHAT <akshatpandey.2711@gmail.com>
2025-04-11 16:44:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,28 @@
+let score ="akshat"
+
+//console.log(typeof score)
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" =>NaN    //not a number
+// true => 1
+// false => 0
+
+//let isLoggedIn = "" //kuch nahi likhte tab bhi answer aata hai isme false aayega
+
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Akshat"=> true
+
+let number = 40
+
+let stringnumber = String(number)
+console.log(stringnumber);
+console.log(typeof stringnumber)
