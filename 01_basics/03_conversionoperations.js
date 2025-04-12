d7ab8391eceafd1d2d203b2c4768b45427564405
AKSHAT <akshatpandey.2711@gmail.com>
2025-04-12 06:15:34 +0000

    convesionoperations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -42,8 +42,28 @@ let number = 40
 // console.log(2%2);
 
 
-let str1 = "Hello"
-let str2 = " Akshat"
+// let str1 = "Hello"
+// let str2 = " Akshat"
 
-let str3 = str1 + str2 
-console.log(str3);
+// let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3")
+
+console.log(+true);
+console.log (+"")
+
+let num1, num2, num3
+num1=num2=num3=2 + 2
+
+let gameCounter = 1000
+++gameCounter;
+console.log(gameCounter);
+
+//link to study
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
