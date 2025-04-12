feda903ac76fb888556ef0218e2ce7f611dd7fe5
AKSHAT <akshatpandey.2711@gmail.com>
2025-04-12 06:33:55 +0000

    conparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log(1 < "2");   
+
+// we must ensure that data type must be same when comparing two numbers.
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(typeof null);
+
+// console.log(undefined = 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//===
+
+// console.log(1 === "2");
+
+// these conversions can create problems
